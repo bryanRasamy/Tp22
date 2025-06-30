2e42cfdb46b1d2edbf564b024e97e3c77ec2fb65
bryanRasamy <bryanrasamiaramanana@gmail.com>
2025-06-30 14:04:31 +0300

    changement dans recheche 2

--- pages/resultatrecherche.php
+++ pages/resultatrecherche.php
@@ -3,19 +3,19 @@
 ?>
 <section class="row">
     <article class="row">
-        <table border="1">
+        <table border="1" class="table table-striped table-hover custom-striped-tab">
             <tr>
                 <td>Departement</td>
                 <td>Nom</td>
                 <td>Prenom</td>
-                <td>Date de naissance</td>
+                <td>Age</td>
             </tr>
             <?php foreach( $recherche as $ligne) { ?>
                 <tr>
                     <td> <?php echo $ligne['dept_name']; ?></td>
                     <td> <?php echo $ligne['last_name'];?></td>
                     <td> <?php echo $ligne['first_name']; ?></td>
-                    <td> <?php echo $ligne['birth_date']; ?></td>
+                    <td> <?php echo $ligne['age']; ?> ans</td>
                 </tr>
             <?php } ?>
         </table>
